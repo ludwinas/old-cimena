284127ca34a0dd6c0b7c40b2acae4f88500ce795
Laura Macchini <l.macchini@thauris.nl>
2015-04-02 11:53:19 +0200

    removed existing lein project files

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: java $JVM_OPTS -cp target/cimena.jar clojure.main -m cimena.core
--- migrations/201503162216-add-movies-table.up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE movies
-(id VARCHAR(20) PRIMARY KEY,
- name VARCHAR(30),
- imdb_link VARCHAR(120),
- is_watched BOOLEAN,
- in_progress BOOLEAN,
- description VARCHAR);
