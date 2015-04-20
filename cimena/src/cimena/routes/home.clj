38b31a52c08e23ddacee770caef7de825964921f
Laura Macchini <l.macchini@thauris.nl>
2015-04-20 16:50:04 +0200

    moved movie-related logic and functions to a separate namespace
    at this point routes.home only contains the function that interact with
    the frontend

--- cimena/src/cimena/routes/home.clj
+++ cimena/src/cimena/routes/home.clj
@@ -2,6 +2,7 @@
   (:require [cimena.layout :as layout]
             [cimena.db.core :as db]
             [cimena.db.helpers :as dbh]
+            [cimena.movie.core :as m]
             [cimena.lib.util :as util]
             [compojure.core :refer [defroutes GET POST]]
             [bouncer.core :as b]
@@ -10,100 +11,6 @@
             [ring.util.response :refer [redirect response]]))
 
 (timbre/refer-timbre) ;; provides timbre aliases in this ns
-
-(defn is-a-movie? [id]
-  (util/not-nil? (dbh/movie-or-nil id)))
-
-(defn format-prev-next [item prev-item next-item]
-  {:movie_id (util/int-or-nil item)
-   :position_prev (util/int-or-nil prev-item)
-   :position_next (util/int-or-nil next-item)})
-
-(defn unique-ids [coll]
-  "creates a set of movie_id values from a collection of maps, we'll use this to
-  compare the sets of ids and determine the difference"
-  (->>  (map :movie_id coll)
-        (into #{})))
-
-(defn split-inserts [old new]
-  "splits an old and a new collection between existing id values and new id
-  values"
-  (let [ids (clojure.set/difference (unique-ids new) (unique-ids old))]
-    ;; in this case contains? can be used to check set membership
-    (group-by (fn [x] (contains? ids (:movie_id x))) new)))
-
-(defn get-item-from-movies [movies movie-id]
-  (util/get-item-with-keyword :id movie-id movies))
-
-(defn get-item-from-movie-positions [movie-positions movie-position-id]
-  (util/get-item-with-keyword :movie_id movie-position-id movie-positions))
-
-(defn get-next [position movie-positions]
-  (let [next-id (:position_next position)]
-    (get-item-from-movie-positions movie-positions next-id)))
-
-(defn walk-positions [current movie-positions]
-  (when current ;; to avoid trying to get the next one when current is nil
-    (cons (:movie_id current)
-          (walk-positions (get-next current movie-positions) movie-positions))))
-
-(defn get-order-from-movie-positions [movie-positions]
-  (let [first-position (some #(when (-> % :position_prev (= nil)) %) movie-positions)]
-        (walk-positions first-position movie-positions)))
-
-(defn sort-movies [movies movie-positions]
-  (let [order (get-order-from-movie-positions movie-positions)]
-    ;; now that I have an ordered list of the movie-ids I can just create an
-    ;; ordered list
-    (map (fn movie-from-index [current-id]
-            (get-item-from-movies movies current-id))
-          order)))
-
-(defn find-obsolete [old-movie-positions movies]
-  (let [old-ids (map :movie_id old-movie-positions)
-        new-ids (map :movie_id movies)]
-    (set-difference old-ids new-ids)))
-
-(defn validate-movie [params]
-  (first
-   (b/validate
-    params
-    :title v/required
-    :link v/required)))
-
-(defn store-positions-from-list! [movie-ids]
-  ;; Here I'll make a linked list of the parameters I got from the frontend
-  (let [movies (vec movie-ids)
-        prev-movies (cons nil movies)
-        next-movies (drop 1 (conj movies nil))
-        movie-positions (into #{} (map format-prev-next movies prev-movies next-movies))
-        old-movie-positions (into #{} (db/get-movie-positions))
-        ;; split-inserts groups the list between new id values (that are
-        ;; missing from the old-movie-positions list) and all the other ones.
-        ;; The maps that contains ids missing from old-movie-positions are sure
-        ;; to be inserts.
-        updates-inserts (split-inserts old-movie-positions movie-positions)
-        inserts (get updates-inserts true) ;; true = insert
-        ;; To determine instead which are updates and which can be omitted, we
-        ;; must compare the update candidates with the existing list
-        updates (util/set-difference (get updates-inserts false) old-movie-positions)
-        obsolete (find-obsolete old-movie-positions movie-positions)]
-    ;; We'll first deal with the inserts
-    (doall (map db/create-movie-position! inserts))
-    ;; then updates
-    (doall (map db/update-movie-position! updates))
-    ;; then we'll clean up those records in the movie-position table
-    ;; that are obsolete
-    (doall (map dbh/delete-movie-position! obsolete))))
-
-(defn archive-movie! [id]
-  "archiving a movie is the process of removing it from the ordered
-  list. specifically we retrieve the ordered list, remove the provided id, then
-  reorder the list"
-  (let [movie-positions (db/get-movie-positions)
-        order (get-order-from-movie-positions movie-positions)
-        new-order (filter #(not= % id) order)]
-    (store-positions-from-list! new-order)))
 
 (defn update-movie! [params]
   ;; when the movie's new state contains is_watched = true
@@ -115,8 +22,15 @@
         ;; In this case we're marking an ordered movie as watched, this means
         ;; that we should remove it from the ordered list, as it's pointless to
         ;; have watched movies in there
-        (archive-movie! movie-id))))
+        (m/archive-movie! movie-id))))
   (db/update-movie! params))
+
+(defn validate-movie [params]
+  (first
+   (b/validate
+    params
+    :title v/required
+    :link v/required)))
 
 (defn save-movie! [{:keys [params]}]
   (if-let [errors (validate-movie params)]
@@ -129,7 +43,9 @@
             (assoc (select-keys params [:title :link])
                    :is_watched (util/not-nil? (:is_watched params))
                    :id (util/int-or-nil (:id params)))]
-        (if (is-a-movie? (:id params))
+        (if (m/is-a-movie? (:id params))
+          ;; we use the function because some archiving logic is necessary before
+          ;; we perform the actual db query
           (update-movie! query-params)
           (db/create-movie! query-params)))
       (redirect "/"))))
@@ -144,7 +60,7 @@
    ;; here I have to sort
    (let [movies (db/get-movies)
          movie-positions (db/get-movie-positions)
-         movies-sorted (sort-movies movies movie-positions)
+         movies-sorted (m/sort-movies movies movie-positions)
          ;; movies-unsorted is the list of movies in the "inbox" area
          ;; the ones that I have just created and I haven't ordered yet
          ;; they are thus the ones that exist, but are missing from the ordered list
@@ -154,17 +70,14 @@
      {:movies movies-sorted :movies-unsorted movies-unsorted})))
 
 (defn edit-movie [{:keys [params flash]}]
-  (let [movie-id (util/int-or-nil (:id params))]
-    (let [movie (-> {:id [movie-id]}
-                    (db/get-movie)
-                    (first))]
+  (let [movie (dbh/movie-or-nil (:id params))]
       (layout/render "movie-edit.html" (merge
                                         {:movie movie}
-                                        (select-keys flash [:errors]))))))
+                                        (select-keys flash [:errors])))))
 
 (defn update-positions! [{:keys [params]}]
   (let [movie-ids (:positions params)]
-    (store-positions-from-list! movie-ids))
+    (m/store-positions-from-list! movie-ids))
   (response {:status "OK"}))
 
 (defn list-watched [{:keys [flash]}]
